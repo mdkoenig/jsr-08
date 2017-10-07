d16a12bae8e420ab6718b0b746cf5b047ca1c085
Sonyl Nagale <sonyl.nagale@nbcuni.com>
2017-10-07 10:53:44 -0400

    Customizing

--- jquery_events/main.js
+++ jquery_events/main.js
@@ -1,13 +1,13 @@
 /* Independent Practice
 
-Making a favorites list: jQuery
+Making a list: jQuery
 
 
-You'll add the ability to complete tasks in your favorite things list:
+You'll add the ability to complete the you gotta be's in your lyrics list:
 
-- Using jQuery, add a "complete task" link at the end of each to-do item (i.e. each "favorite thing")
+- Using jQuery, add a "complete" link at the end of each to-do item (i.e. each "gotta-be")
 - When clicked, the link will cross out the current item (hint: add a class to the list that sets the text-decoration to line-through)
-- Each new item added by the user needs to also have the "complete task" link at the end
+- Each new item added by the user needs to also have the "complete" link at the end
 
 */
 
@@ -16,33 +16,3 @@ function addToList($list, thing) {
   $list.append($thingLi);
   addCompleteLinks($thingLi);
 }
-
-function addCompleteLinks($lis) {
-  var $completedLink = $('<span>').html(' complete task').addClass('complete-task');
-  $lis.append($completedLink);
-
-  $('.complete-task').on('click', function(event) {
-    // We don't need `event.preventDefault()` here
-    // because there is no weird default action
-    // when clicking on a `span` element.
-    $(this).parent().addClass('completed');
-  });
-}
-
-var $thingList = $('#fav-list');
-var $button = $('#new-thing-button');
-var $newThingInput = $('#new-thing');
-
-var $thingListItems = $('#fav-list .fav-thing');
-addCompleteLinks($thingListItems);
-
-$button.on('click', function(event) {
-  event.preventDefault();
-  var newThing = $newThingInput.val();
-  if (newThing === '') {
-    alert('You must type in a value!');
-  } else {
-    addToList($thingList, newThing);
-    $newThingInput.val('');
-  }
-});
